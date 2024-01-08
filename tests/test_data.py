bb4969426705df5eddea587b16f25c217006bdd9
eskehaack <eskehaack@gmail.com>
2024-01-08 10:52:39 +0100

    fixed data dependecy

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,12 @@
+import os
+import pytest
 from mlops_ehaa.data.dataload import mnist
 
 
+@pytest.mark.skipif(
+    not os.path.exists("data/processed/corruptmnist/train_set.pth"),
+    reason="Data files not found",
+)
 def test_mnist():
     bt_size = 8
     out = mnist(batch_size=bt_size)
